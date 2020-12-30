3e0ccb006a254362aee25a9fe7b66fd8f6685bda
Chase Hanson <hallsville3@gmail.com>
2020-12-30 03:53:04 -0600

    Fixed screen size to fit all content on any device

--- src/gameboy/hallsville3/Main.java
+++ src/gameboy/hallsville3/Main.java
@@ -5,7 +5,7 @@ import java.io.IOException;
 public class Main {
     public static void main(String[] args) throws IOException, InterruptedException {
         GameBoy gb = new GameBoy();
-        String game = "ROMs/cpu_instrs.gb";
+        String game = "ROMS/kirby.gb";
         //game = "ROMs/zelda.gb";
         //game = "ROMs/mario.gb";
         //game = "ROMs/mario2.gb";
@@ -13,7 +13,6 @@ public class Main {
         //game = "ROMs/doctor.gb";
         //game = "ROMs/dmg-acid2.gb";
         //game = "ROMs/cpu_instrs/individual/2.gb";
-        //game = "ROMs/kirby.gb";
         //game = "ROMs/pokemon.gb";
         gb.loadGame(game);
         gb.run();
--- src/gameboy/hallsville3/Window.java
+++ src/gameboy/hallsville3/Window.java
@@ -5,14 +5,16 @@ import java.awt.*;
 
 public class Window {
     JFrame frame;
+    static int barHeight = 28;
     public Window(PPU ppu, int scale) {
         frame = new JFrame("GameBoy");
-        frame.setSize(160 * scale, 144 * scale + 28);
         frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
         frame.setResizable(false);
 
         Screen screen = new Screen(ppu, scale);
+        screen.setPreferredSize(new Dimension(160 * scale, 144 * scale));
         frame.add(screen);
+        frame.pack();
 
         frame.setVisible(true);
     }
