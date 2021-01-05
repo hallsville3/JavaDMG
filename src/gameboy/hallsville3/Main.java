b72d9482d2dc2b6948088c40be6273da7c14ec34
Chase Hanson <hallsville3@gmail.com>
2021-01-04 18:23:57 -0600

    Add gitignore

--- src/gameboy/hallsville3/Main.java
+++ src/gameboy/hallsville3/Main.java
@@ -7,7 +7,7 @@ public class Main {
         GameBoy gb = new GameBoy();
         String game = "ROMS/kirby.gb";
         //game = "ROMs/zelda.gb";
-        //game = "ROMs/mario.gb";
+        game = "ROMs/mario.gb";
         //game = "ROMs/halt_bug.gb";
         //game = "ROMs/mario2.gb";
         //game = "ROMs/tetris.gb";
